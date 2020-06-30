4828626170f819f95300f449fc4ce57d4c608fb7
AntonGrn <45007883+AntonGrn@users.noreply.github.com>
2020-06-30 17:48:05 +0200

    Gadget example: ESP8266 sensor

---
+++ home-server/gadget-examples/ESP8266_SENSOR_VALUE/ESP8266_SENSOR_VALUE.ino
@@ -0,0 +1,113 @@
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+
+// WiFi connection
+const char* ssid     = "XXXXX";
+const char* password = "XXXXX";
+
+// TCP Server socket
+const int port = 8082;
+WiFiServer wifiServer(port);
+
+// DHT temperature & humidity sensor
+#define DHTTYPE DHT11
+#define DHTPIN 2
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+
+  Serial.begin(115200);
+
+  //Set up as station
+  WiFi.mode(WIFI_STA);
+  // Network device name (optional)
+  WiFi.hostname("ALMA ESP gadget");
+  // Initialize the WiFi instence
+  WiFi.begin(ssid, password);
+
+  //Connect to network
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("Connecting to WiFi: ");
+    Serial.println(ssid);
+  }
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Initialize the socket server instance
+  wifiServer.begin();
+
+  // Initialize the DHT instance
+  dht.begin();
+}
+
+void loop() {
+  //Reonnect to network if connection lost.
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Recnnecting to WiFi");
+    delay(1000);
+  }
+
+  // Poll to check if socket client is available
+  // Non-blocking.
+  // Returns a WiFiClient object
+  WiFiClient client = wifiServer.available();
+
+  // If the returned WiFiClient is valid:
+  if (client) {
+    Serial.println("Valid client connected");
+    // While the client is connected:
+    while (client.connected()) {
+      // While there are something to read from the client:
+      while (client.available() > 0) { // Returns 0 until you read something
+        Serial.println("Message from home server available:");
+        String serverRequest = client.readStringUntil('\n');
+        Serial.print("Command: ");
+        Serial.println(serverRequest);
+
+        // Process server message according to ALMA communication protocol
+        if (serverRequest.startsWith("15")) {
+          // 15: Poll request.
+          // Derive request_spec from ALMA command, format: 15:request_spec
+          int indexOfColon = serverRequest.indexOf(":");
+          int fromIndex = indexOfColon + 1;
+          String request_spec = serverRequest.substring(fromIndex);
+          Serial.print("request_spec: ");
+          Serial.println(request_spec);
+          String requested_data = "0";
+          if (request_spec.equals("temperature")) {
+            requested_data = getTemperature();
+          } else if (request_spec.equals("humidity")) {
+            requested_data = getHumidity();
+          }
+          // Respond with requested data
+          String response = "16:" + requested_data;
+          client.println(response);
+          client.flush();
+        }
+      }
+      delay(10);
+    }
+    client.stop();
+  }
+  delay(500);
+}
+
+String getTemperature() {
+  float temperature = dht.readTemperature();
+  String stringTemp = String(temperature);
+  int indexOfDecimal = stringTemp.indexOf(".");
+  Serial.print(stringTemp);
+  Serial.println(" C");
+  return stringTemp.substring(0, indexOfDecimal);
+}
+
+String getHumidity() {
+  float humidity = dht.readHumidity();
+  String stringHumid = String(humidity);
+  int indexOfDecimal = stringHumid.indexOf(".");
+  Serial.print(stringHumid);
+  Serial.println(" %");
+  return stringHumid.substring(0, indexOfDecimal);
+}
---
+++ home-server/gadget-examples/ESP8266_SENSOR_VALUE/README.txt
@@ -0,0 +1,40 @@
+A native SENSOR_VALUE ALMA gadget to monitor ESP8266 sensor values.
+
+NOTE
+This gadget unit provides 2 gadget services:
+- Temperature sensor.
+- Humidity sensor.
+Each gadget service is represented by its own gadget record in 
+'gadgets.json', by specifying 'request_spec' (see example below).
+
+HARDWARE REQUIREMENTS:
+- ESP8266 WiFi microchip
+- DHT11 Temperature & humidity sensor
+ 
+SETUP:
+1. In code: Assign values to ssid and password (for network connection).
+2. Upload code ESP8266_SENSOR_VALUE to ESP8266 WiFi microchip.
+3. Run ESP8266 to capture its IP-address (for gadget setup in 'gadgets.json')
+4. In HomeServer:
+   - Add gadget record(s) in 'gadgets.json', under "alma".
+   e.g:
+   {
+     "gadget_id": 3,
+     "alias": "Temperature (C)",
+     "type": "SENSOR_VALUE",
+     "poll_delay_seconds": 30,
+     "enabled": true,
+     "IP_address": "192.168.0.13",
+     "TCP_port": 8082,
+     "request_spec": "temperature"
+   },
+   {
+     "gadget_id": 4,
+     "alias": "Humidity (%)",
+     "type": "SENSOR_VALUE",
+     "poll_delay_seconds": 60,
+     "enabled": true,
+     "IP_address": "192.168.0.13",
+     "TCP_port": 8082,
+     "request_spec": "humidity"
+   }
